2d9cc25a68f3cd0d542ae24acc31e5e88deab1b4
anh.do <tuananh9h@gmail.com>
2024-06-05 16:09:02 +0700

    Query parameters

--- src/index.js
+++ src/index.js
@@ -25,6 +25,11 @@ app.get("/news", (req, res) => {
   res.render("news");
 });
 
+app.get("/search", (req, res) => {
+  console.log("query: ", req.query.q);
+  res.render("search");
+});
+
 // 127.0.0.1 ~ localhost
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
