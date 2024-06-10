d90c34fbd85e3bf4b3c555bbc6803fe8df23dffc
anh.do <tuananh9h@gmail.com>
2024-06-10 11:29:42 +0700

    Trick rename sortMiddleware

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require("morgan");
 const methodOverride = require("method-override");
 const { engine } = require("express-handlebars");
 
-const SortMiddleware = require("./app/middleware/SortMiddleware");
+const SortMiddleware = require("./app/middlewares/sortMiddleware.x");
 
 const app = express();
 const port = 3000;
@@ -38,28 +38,7 @@ app.engine(
   "hbs",
   engine({
     extname: ".hbs",
-    helpers: {
-      sum: (a, b) => a + b,
-      sortable: (field, sort) => {
-        const sortType = field === sort.column ? sort.type : "default";
-        const icons = {
-          default: "elevator-icon",
-          asc: "sort-ascending-icon",
-          desc: "sort-descending-icon"
-        };
-        const types = {
-          default: "desc",
-          asc: "desc",
-          desc: "asc"
-        };
-
-        const icon = icons[sortType];
-        const type = types[sortType];
-        return `<a href="?_sort&column=${field}&type=${type}">
-        <span class="${icon}"></span>
-      </a>`;
-      }
-    }
+    helpers: require("./helpers/handlebars")
   })
 );
 app.set("view engine", "hbs");
